6a0dfc368d1c5ea606a11be380268955a533cd30
SOUL X SYSTEM <154720925+mahtoanjali@users.noreply.github.com>
2024-01-02 07:19:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,6 +11,6 @@ import os
 
 
 class Config(object):
-    API_ID = int(os.environ.get("API_ID", 12345678))
+    API_ID = int(os.environ.get("API_ID", 21011056))
     API_HASH = os.environ.get("API_HASH")
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
